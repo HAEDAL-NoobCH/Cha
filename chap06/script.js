d93ab76dc1575623ddd288bce9794245223b37b8
ss0526100 <ss0526100@gmail.com>
2023-07-11 09:35:01 +0900

    chap06

--- chap06/script.js
+++ chap06/script.js
@@ -8,13 +8,10 @@ let header = document.querySelector('header');
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
-})
 
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
+    let height = window.innerWidth;
     stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
+    moon.style.top = -250 + value * 3 + 'px';
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
     text.style.marginRight = value * 4 + 'px';
@@ -22,3 +19,14 @@ window.addEventListener('scroll', function() {
     btn.style.marginTop = value * 1.5 + 'px';
     header.style.top = value * 0.5 + 'px';
 })
+let value = window.scrollY;
+
+let height = window.innerWidth;
+stars.style.left = value * 0.25 + 'px';
+moon.style.top = -300 + value * 3 + 'px';
+mountains_behind.style.top = value * 0.5 + 'px';
+mountains_front.style.top = value * 0 + 'px';
+text.style.marginRight = value * 4 + 'px';
+text.style.marginTop = value * 1.5 + 'px';
+btn.style.marginTop = value * 1.5 + 'px';
+header.style.top = value * 0.5 + 'px';
